f7eedf0e6773c8a4cf446acf5b07707dc767b2ba
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-02-02 15:44:42 -0600

    formatting and comment complete

--- lab5/app.js
+++ lab5/app.js
@@ -3,7 +3,7 @@
  * This is the only file you should edit!
  */
 
-
+// do function after page loaded
 $(document).ready(function () {
 
     $('#remove_deprecated').click(function () { strikeOutAndDisableLink() });
@@ -14,6 +14,11 @@ $(document).ready(function () {
     $('#hide_annoying').click(function () { hideAllAnnoyAnnounce() });
     $('#show_annoying').click(function () { showAllAnnoyAnnounce() });
 
+    /**
+     * [strikeOutAndDisableLink strike out selected anchors and prevent opening links]
+     *
+     * @return  {[void]}  [return void]
+     */
     function strikeOutAndDisableLink() {
 
         $('li.deprecated a').each(function () {
@@ -30,6 +35,11 @@ $(document).ready(function () {
         });
     }
 
+    /**
+     * [colorH2InMainRed make h2 in main red]
+     *
+     * @return  {[void]}  [return void]
+     */
     function colorH2InMainRed() {
         $('div.main h2').css(
             {
@@ -38,6 +48,11 @@ $(document).ready(function () {
         );
     }
 
+    /**
+     * [colorH3InSecondaryBlue make h3 in secondary blue]
+     *
+     * @return  {[void]}  [return void]
+     */
     function colorH3InSecondaryBlue() {
         $('div.secondary h3').css(
             {
@@ -46,6 +61,11 @@ $(document).ready(function () {
         );
     }
 
+    /**
+     * [boldFirstParagraphInMain make 1st paragraph in main bold]
+     *
+     * @return  {[void]}  [return void]
+     */
     function boldFirstParagraphInMain() {
         $('div.main>p:first-of-type').css(
             {
@@ -54,6 +74,11 @@ $(document).ready(function () {
         );
     }
 
+    /**
+     * [boldLastParagraphInSecondary make last paragraph in secondary bold]
+     *
+     * @return  {[void]}  [return void]
+     */
     function boldLastParagraphInSecondary() {
         $('div.secondary>p:last-of-type').css(
             {
@@ -62,10 +87,20 @@ $(document).ready(function () {
         );
     }
 
+    /**
+     * [hideAllAnnoyAnnounce hide all annoying announcements]
+     *
+     * @return  {[void]}  [return void]
+     */
     function hideAllAnnoyAnnounce() {
         $('.annoying').fadeOut('slow');
     }
 
+    /**
+     * [showAllAnnoyAnnounce show all annoying announcements]
+     *
+     * @return  {[void]}  [return void]
+     */
     function showAllAnnoyAnnounce() {
         $('.annoying').fadeIn('slow');
     }
