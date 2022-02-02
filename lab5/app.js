34b86610e61d1c71c772758c012829e81098ae72
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-02-02 15:27:44 -0600

    complete add click handlers

--- lab5/app.js
+++ lab5/app.js
@@ -5,69 +5,69 @@
 
 
 $(document).ready(function () {
-    strikeOutAndDisableLink();
-    colorH2InMainRed();
-    colorH3InSecondaryBlue();
-    boldFirstParagraphInMain();
-    boldLastParagraphInSecondary();
-    hideAllAnnoyAnnounce();
-    showAllAnnoyAnnounce();
-});
 
+    $('#remove_deprecated').click(function () { strikeOutAndDisableLink() });
+    $('#main_h2').click(function () { colorH2InMainRed() });
+    $('#secondary_h3').click(function () { colorH3InSecondaryBlue() });
+    $('#bold_p_main').click(function () { boldFirstParagraphInMain() });
+    $('#bold_p_secondary').click(function () { boldLastParagraphInSecondary() });
+    $('#hide_annoying').click(function () { hideAllAnnoyAnnounce() });
+    $('#show_annoying').click(function () { showAllAnnoyAnnounce() });
 
-function strikeOutAndDisableLink() {
+    function strikeOutAndDisableLink() {
 
-    $('li.deprecated a').each(function () {
-        $(this).css(
+        $('li.deprecated a').each(function () {
+            $(this).css(
+                {
+                    'color': '#900',
+                    'text-decoration': 'line-through'
+                }
+            );
+
+            $(this).click(function (e) {
+                e.preventDefault();
+            });
+        });
+    }
+
+    function colorH2InMainRed() {
+        $('div.main h2').css(
             {
-                'color': '#900',
-                'text-decoration': 'line-through'
+                'color': 'red'
             }
         );
+    }
 
-        $(this).click(function (e) {
-            e.preventDefault();
-            
-        });
-    });
-}
+    function colorH3InSecondaryBlue() {
+        $('div.secondary h3').css(
+            {
+                'color': 'blue'
+            }
+        );
+    }
 
-function colorH2InMainRed() {
-    $('div.main h2').css(
-        {
-            'color': 'red'
-        }
-    );
-}
+    function boldFirstParagraphInMain() {
+        $('div.main>p:first-of-type').css(
+            {
+                'font-weight': '700'
+            }
+        );
+    }
 
-function colorH3InSecondaryBlue() {
-    $('div.secondary h3').css(
-        {
-            'color': 'blue'
-        }
-    );
-}
+    function boldLastParagraphInSecondary() {
+        $('div.secondary>p:last-of-type').css(
+            {
+                'font-weight': '700'
+            }
+        );
+    }
 
-function boldFirstParagraphInMain() {
-    $('div.main>p:first-of-type').css(
-        {
-            'font-weight':'700'
-        }
-    );
-}
+    function hideAllAnnoyAnnounce() {
+        $('.annoying').fadeOut('slow');
+    }
 
-function boldLastParagraphInSecondary() {
-    $('div.secondary>p:last-of-type').css(
-        {
-            'font-weight':'700'
-        }
-    );
-}
+    function showAllAnnoyAnnounce() {
+        $('.annoying').fadeIn('slow');
+    }
 
-function hideAllAnnoyAnnounce() {
-    $('.annoying').fadeOut('slow');
-}
-
-function showAllAnnoyAnnounce() {
-    $('.annoying').fadeIn('slow');
-}
+});
