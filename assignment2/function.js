4accfd9885b1e441a347cd4630a7ee913dc7cb42
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-02-04 05:16:08 -0600

    medium text func complete

--- assignment2/function.js
+++ assignment2/function.js
@@ -3,10 +3,15 @@ $(document).ready(function () {
 
     // set click handlers
     $('#small').click(function () { smallText(); });
+    $('#medium').click(function () { mediumText(); });
 
     // ********* FUNCTIONS ***********
     function smallText() {
         $('#text_box').css({ 'font-size': '1em' });
+    }
+
+    function mediumText() {
+        $('#text_box').css({ 'font-size': '1.3em' });
     }
 
 });
