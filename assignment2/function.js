c0873bd1f267289dfcd36804b8c44feb10d01418
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-02-04 06:17:45 -0600

    format and comment complete

--- assignment2/function.js
+++ assignment2/function.js
@@ -10,22 +10,48 @@ $(document).ready(function () {
     $('#x_button').click(function () { fadeOutSlideBox(); });
 
     // ********* FUNCTIONS ***********
+    
+    /**
+     * [smallText make text size to 12px]
+     *
+     * @return  {[void]}  [return void]
+     */
     function smallText() {
         $('#text_box').css({ 'font-size': '12px' });
     }
 
+    /**
+     * [mediumText make text size to 16px]
+     *
+     * @return  {[void]}  [return void]
+     */
     function mediumText() {
         $('#text_box').css({ 'font-size': '16px' });
     }
 
+    /**
+     * [largeText make text size to 20px]
+     *
+     * @return  {[void]}  [return void]
+     */
     function largeText() {
         $('#text_box').css({ 'font-size': '20px' });
     }
 
+    /**
+     * [toggleSwitch toggle buttons to show or hide]
+     *
+     * @return  {[void]}  [return void]
+     */
     function toggleSwitch() {
         $('#text_btns').toggle('slow');
     }
 
+    /**
+     * [slideBox make slideBox move rightward and fadein]
+     *
+     * @return  {[void]}  [return void]
+     */
     function slideBox() {
         $("#slide_box").animate(
 
@@ -37,6 +63,11 @@ $(document).ready(function () {
             "slow");
     }
 
+    /**
+     * [fadeOutSlideBox make slideBox move leftward and fadeout]
+     *
+     * @return  {[void]}  [return void]
+     */
     function fadeOutSlideBox() {
         $("#slide_box").animate(
 
@@ -47,6 +78,4 @@ $(document).ready(function () {
 
             "slow");
     }
-
-    
 });
