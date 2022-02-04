6c3b902cab0438d9596c68580ce810e145c93c24
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-02-04 06:03:09 -0600

    slideBox func complete

--- assignment2/function.js
+++ assignment2/function.js
@@ -6,21 +6,33 @@ $(document).ready(function () {
     $('#medium').click(function () { mediumText(); });
     $('#large').click(function () { largeText(); });
     $('#toggle_switch').click(function () { toggleSwitch(); });
+    $('#slide_box_btn').click(function () { slideBox(); });
 
     // ********* FUNCTIONS ***********
     function smallText() {
-        $('#text_box').css({ 'font-size': '1em' });
+        $('#text_box').css({ 'font-size': '12px' });
     }
 
     function mediumText() {
-        $('#text_box').css({ 'font-size': '1.3em' });
+        $('#text_box').css({ 'font-size': '16px' });
     }
 
     function largeText() {
-        $('#text_box').css({ 'font-size': '1.6em' });
+        $('#text_box').css({ 'font-size': '20px' });
     }
 
     function toggleSwitch() {
         $('#text_btns').toggle('slow');
+    }
+
+    function slideBox() {
+        $("#slide_box").animate(
+
+            {
+                "left": "+=500px",
+                opacity: 1
+            },
+
+            "slow");
     }
 });
